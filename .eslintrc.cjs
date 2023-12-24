ff49f4992f0b0d1aa20cd463df976707cd28024a
Ryu <privetslavka@yandex.com>
2023-12-24 15:27:21 +0300

    Добавляет базовые настройки

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,30 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'htmlacademy/react-typescript',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+  },
+  settings: { react: { version: 'detect' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'check-file/filename-naming-convention': [
+      'error',
+      { 'src/components/**/*.{jsx,tsx,js,ts}': 'PASCAL_CASE' },
+    ],
+    'check-file/folder-naming-convention': [
+      'error',
+      { 'src/components/**/*': 'PASCAL_CASE' },
+    ],
   },
-}
+};
---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+
+function App() {
+  return <img className="peach-gif" src="peach.gif" />;
+}
+
+export default App;
